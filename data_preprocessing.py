e93370f3d13f7ca41112fb901a432e7bf5e66c6b
Alice Sun <lingmuchezi_alice@126.com>
2022-07-24 23:31:06 -0400

    Add files via upload

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 
+# Reading data
 # MovieID, Title, Genres
 movie_data = pd.DataFrame([movie.replace('\n','').split('::') for movie in open('movies.txt', encoding="ISO-8859-1").readlines()],
                           columns=['MovieID', 'Title', 'Genres'])
@@ -9,18 +10,26 @@ movie_data = pd.DataFrame([movie.replace('\n','').split('::') for movie in open(
 user_data = pd.DataFrame([user.replace('\n','').split('::') for user in open('users.txt', encoding="ISO-8859-1").readlines()],
                          columns=['UserID', 'Gender', 'Age', 'Occupation', 'Zip-code'])
 user_data['Age'] = user_data['Age'].astype(int)
-# user_validation = np.random.choice(user_data['UserID'], 5)
 
 # UserID, MovieID, Rating, Timestamp
 rating_data = pd.DataFrame([rating.replace('\n','').split('::') for rating in open('ratings.txt', encoding="ISO-8859-1").readlines()],
                            columns=['UserID', 'MovieID', 'Rating', 'Timestamp'])
 rating_data['Rating'] = rating_data['Rating'].astype(int)
 rating_data['Timestamp'] = pd.to_datetime(rating_data['Timestamp'], unit='s')
-rating_data['Rank_Latest'] = rating_data.groupby(['UserID'])['Timestamp'].rank(method='first',ascending=False)
 
-# dataset 3: validation user set
+
+# Selecting Data
+# validation set (5 users)
+# user_validation = np.random.choice(user_data['UserID'], 5)
 user_validation = ['5985' '3280' '5126' '907' '106']
-# dataset 1: training data set # 994169 6040
-training_rating = rating_data[rating_data['Rank_Latest'] != 1][~rating_data['UserID'].isin(user_validation)]
-# dataset 2: testing data set
-testing_rating = rating_data[rating_data['Rank_Latest'] == 1][~rating_data['UserID'].isin(user_validation)]
+# dataset for training and testing
+rating_data_selected = rating_data[~rating_data['UserID'].isin(user_validation)]
+rating_data_selected['Rank_Latest'] = rating_data.groupby(['UserID', 'Rating'])['Timestamp'].rank(method='first',ascending=False)
+
+
+# dataset 1: training dataset
+training_rating = rating_data_selected[rating_data_selected['Rank_Latest'] != 1]
+# dataset 2: testing dataset
+testing_rating = rating_data_selected[rating_data_selected['Rank_Latest'] == 1]
+# dataset 3: validation dataset (5 users)
+user_validation = ['5985' '3280' '5126' '907' '106']
